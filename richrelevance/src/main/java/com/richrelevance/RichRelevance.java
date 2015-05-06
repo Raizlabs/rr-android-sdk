896fa3386ef308b7c6a2c1f18fc01d5876369321
Dylan James <dylanrjames@gmail.com>
2015-05-06 15:24:27 -0400

    Base project structure with test app.

---
+++ richrelevance/src/main/java/com/richrelevance/RichRelevance.java
@@ -0,0 +1,10 @@
+package com.richrelevance;
+
+import android.util.Log;
+
+public class RichRelevance {
+
+    public void doSomething() {
+        Log.i(getClass().getSimpleName(), "Okay, I did it.");
+    }
+}
