2103170ae3d923589f8f990ffbbd866c88aacd45
Dylan James <dylanrjames@gmail.com>
2015-06-23 14:59:17 -0400

    Added integration endpoint.

--- richrelevance/src/main/java/com/richrelevance/Endpoints.java
+++ richrelevance/src/main/java/com/richrelevance/Endpoints.java
@@ -5,4 +5,5 @@ package com.richrelevance;
  */
 public class Endpoints {
     public static final String PRODUCTION = "recs.richrelevance.com";
+    public static final String INTEGRATION = "integration.richrelevance.com";
 }
